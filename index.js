552138a292de3a1cf815b994a41a397cf2267bb8
Richard Kiene <richard.kiene@gmail.com>
2015-11-08 22:33:51 -0800

    Periodically sync stats to file

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 
 var mod_bunyan = require('bunyan');
 var mod_config = require('config');
+var mod_jsonfile = require('jsonfile');
 var mod_slack = require('slack-client');
 
 var token = mod_config.get('slack.token');
@@ -17,8 +18,10 @@ var log = mod_bunyan.createLogger({
         level: process.env['LOG_LEVEL'] || 'error',
         stream: process.stderr
     });
-var demerits = {};
-var merits = {}
+var demeritsPath = './data/demerits.json';
+var meritsPath = './data/merits.json'
+var demerits = mod_jsonfile.readFileSync(demeritsPath);
+var merits = mod_jsonfile.readFileSync(meritsPath);
 
 function main() {
     slack.on('open', function _open() {
@@ -27,10 +30,12 @@ function main() {
 
     slack.on('message', function _message(message) {
         var channel = slack.getChannelGroupOrDMByID(message.channel);
-        log.info('Incoming message: %j in channel %j', message, channel);
+        log.info('Incoming message: %j', message);
 
         var demeritMatch = message.text.match(/([A-Za-z0-9]+):( ?)demerit/);
         var meritMatch = message.text.match(/([A-Za-z0-9]+):( ?)merit/);
+        log.debug('demeritMatch: %j', demeritMatch);
+        log.debug('meritMatch: %j', meritMatch);
         if (demeritMatch) {
             var name = demeritMatch[1];
             demerits[name] = demerits[name] ? demerits[name] + 1 : 1;
@@ -42,7 +47,7 @@ function main() {
             var name = meritMatch[1];
             merits[name] = merits[name] ? merits[name] + 1 : 1;
             var count = merits[name];
-            
+
             log.info('Merit issued to %s', name);
             channel.send('Merit to ' + name + ' (count: ' + count + ')');
         }
@@ -53,6 +58,21 @@ function main() {
     });
 
     slack.login();
+
+    setInterval(function _saveStats() {
+        mod_jsonfile.writeFile(demeritsPath, demerits, function (err) {
+            if (err) {
+                log.error('Error saving demerits: %s', err);
+            }
+        });
+
+        mod_jsonfile.writeFile(meritsPath, merits, function (err) {
+            if (err) {
+                log.error('Error saving merits: %s', err);
+            }
+        });
+
+    }, 5000);
 }
 
 main();
